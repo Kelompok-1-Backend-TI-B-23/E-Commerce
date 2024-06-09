7720148caf0f39d9c7db43ecaea48a6e49470839
eilleendwe <eilleenyeoh3@gmail.com>
2024-06-09 15:43:03 +0700

    [DEV] Membuat tampilan register dan login
    Membuat route dan views

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -1,7 +1,41 @@
 <?php
 
+use App\Http\Controllers\checkoutController;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', [homeController::class, 'index']);
+
+Route::get('/login', [loginController::class, 'index']);
+Route::get('/register', [registerController::class, 'index']);
+Route::get('/checkout', [checkoutController::class, 'index']);
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+// Route::get('/about', function () {
+//    return view('about');
+// });
+
+
+// Route::get('/mahasiswa', function () {
+//     echo 'Mahasiswa';
+// });
+
+
+// Route::get('/user/{id}', function ($id) {
+//     echo "Hello" . $id;
+// });
+
+// Route::get('/users/{name?}', function ($name = 'Peter') {
+//     return ('User name: ' . $name);
+// });
+
+
