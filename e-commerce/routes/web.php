62d0b7a5b4858442862ba0be4f32ccee23be6b6e
eilleendwe <eilleenyeoh3@gmail.com>
2024-06-09 19:37:40 +0700

    [DEV] Membuat route serta tampilan about dan home

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -4,38 +4,16 @@ use App\Http\Controllers\checkoutController;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\registerController;
+use App\Http\Controllers\aboutController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/', [homeController::class, 'index']);
-
 Route::get('/login', [loginController::class, 'index']);
 Route::get('/register', [registerController::class, 'index']);
-Route::get('/checkout', [checkoutController::class, 'index']);
-
-// Route::get('/home', function () {
-//     return view('home');
-// });
-
-// Route::get('/about', function () {
-//    return view('about');
-// });
+Route::get('/about', [aboutController::class, 'index']);
 
 
-// Route::get('/mahasiswa', function () {
-//     echo 'Mahasiswa';
-// });
 
-
-// Route::get('/user/{id}', function ($id) {
-//     echo "Hello" . $id;
-// });
-
-// Route::get('/users/{name?}', function ($name = 'Peter') {
-//     return ('User name: ' . $name);
-// });
 
 
