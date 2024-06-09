9955fb5611f0eaf0b2467f7c48f7bcdf02573719
eilleendwe <eilleenyeoh3@gmail.com>
2024-06-09 19:42:18 +0700

    [DEV] Membuat tampilan change password dan change pin

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\registerController;
 use App\Http\Controllers\aboutController;
 use App\Http\Controllers\profileController;
 use App\Http\Controllers\updateProfileController;
-
+use App\Http\Controllers\changePinController;
+use App\Http\Controllers\changePasswordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [homeController::class, 'index']);
@@ -16,6 +17,7 @@ Route::get('/register', [registerController::class, 'index']);
 Route::get('/about', [aboutController::class, 'index']);
 Route::get('/profile', [profileController::class, 'index']);
 Route::get('/updateProfile', [updateProfileController::class, 'index']);
-
+Route::get('/changePassword', [changePasswordController::class, 'index']);
+Route::get('/changePin', [changePinController::class, 'index']);
 
 
