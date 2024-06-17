1f5d384cb5f43106760bbd15a14bbecf1e7c7be2
James Anderson <jamesandrn@gmail.com>
2024-06-17 15:24:41 +0700

    Update changePassword.blade.php

--- e-commerce/resources/views/changePassword.blade.php
+++ e-commerce/resources/views/changePassword.blade.php
@@ -12,18 +12,19 @@
                 <div class="card-body">
                     <h4 class="card-title text-center mb-4"><b>Change Password</b></h4>
                     <div class="password-change-form">
-                        <form>
+                        <form action="{{ url('/changePassword') }}" method="POST">
+                            @csrf
                             <div class="mb-3">
                                 <label for="currentPassword" class="form-label">Current Password</label>
-                                <input type="password" class="form-control" id="currentPassword" placeholder="Enter current password">
+                                <input type="password" class="form-control" id="currentPassword" name="currentPassword" placeholder="Enter current password" required>
                             </div>
                             <div class="mb-3">
                                 <label for="newPassword" class="form-label">New Password</label>
-                                <input type="password" class="form-control" id="newPassword" placeholder="Enter new password">
+                                <input type="password" class="form-control" id="newPassword" name="newPassword" placeholder="Enter new password" required>
                             </div>
                             <div class="mb-3">
                                 <label for="confirmNewPassword" class="form-label">Confirm New Password</label>
-                                <input type="password" class="form-control" id="confirmNewPassword" placeholder="Confirm new password">
+                                <input type="password" class="form-control" id="confirmNewPassword" name="confirmNewPassword" placeholder="Confirm new password" required>
                             </div>
                             <div class="d-grid gap-2">
                                 <button type="submit" class="btn btn-dark">Change Password</button>
