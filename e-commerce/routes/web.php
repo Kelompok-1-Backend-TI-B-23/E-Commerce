658595cf95500899e9adb6de0b05f15ea66dfee2
James Anderson <jamesandrn@gmail.com>
2024-06-17 14:33:13 +0700

    Update web.php

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\purchasehistoryController;
 use App\Http\Controllers\createAccountController;
 use Illuminate\Support\Facades\Route;
 
+<<<<<<< HEAD
 Route::get('/', [homeController::class, 'index']);
 Route::get('/login', [loginController::class, 'index']);
 Route::get('/about', [aboutController::class, 'index']);
@@ -32,3 +33,41 @@ Route::get('/purchaseHistory', [purchasehistoryController::class, 'index']);
 // Create account
 Route::get('/createAccount', [createAccountController::class, 'index']);
 Route::post('/createAccount', [createAccountController::class, 'createAccount']);
+=======
+
+
+Route::get('/login', [loginController::class, 'index'])->name('indexLogin');
+Route::post('/login', [loginController::class, 'login'])->name('login');
+
+Route::get('/logout', [loginController::class, 'logout'])->name('logout');
+
+// Create account
+Route::get('/createAccount', [createAccountController::class, 'index']);
+Route::post('/createAccount', [createAccountController::class, 'createAccount']);
+
+
+
+// jadi localhost/user/home buat mastiin harus login dlu sebelum masuk website
+Route::group(['prefix' => 'user', 'middleware' => ['auth'], 'as' => 'user.'], function(){
+    Route::get('/home', [homeController::class, 'index'])->name('home');
+    Route::get('/profile', [profileController::class, 'index'])->name('profile');
+    Route::get('/updateProfile', [updateProfileController::class, 'index'])->name('updateProfile');
+    Route::get('/changePassword', [changePasswordController::class, 'index'])->name('changePassword');
+    Route::get('/changePin', [changePinController::class, 'index'])->name('changePin');
+    Route::get('/cart', [cartController::class, 'index'])->name('cart');
+    Route::get('/checkout', [checkoutController::class, 'index'])->name('checkout');
+    Route::get('/topup', [topupController::class, 'index'])->name('topup');
+    Route::get('/purchaseHistory', [purchasehistoryController::class, 'index'])->name('purchaseHistory');
+    Route::get('/about', [aboutController::class, 'index'])->name('about');
+});
+
+
+
+
+
+
+
+
+
+
+>>>>>>> origin/Login
