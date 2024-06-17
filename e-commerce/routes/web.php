12aecd1ca55d68b4ec30889ead79c8e2be7f8d00
James Anderson <jamesandrn@gmail.com>
2024-06-17 15:31:28 +0700

    Update Route Change Password

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -37,3 +37,8 @@ Route::post('/changePin', [changePinController::class, 'changePin']);
 // Change Password
 Route::get('/changePassword', [changePasswordController::class, 'index']);
 Route::post('/changePassword', [changePasswordController::class, 'changePassword']);
+
+
+
+
+
