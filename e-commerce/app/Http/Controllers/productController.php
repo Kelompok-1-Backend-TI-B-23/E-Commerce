bc4a26826fc8d500ebbf2fc0962445ccd0379e8a
jclynviaa <162530118+jclynviaa@users.noreply.github.com>
2024-06-20 22:42:56 +0700

    [UPDATE] Update Fungsi Sort & Filter
    - tambahin product ke seeder buat coba-coba
    - edit sortby jadi cuman buat price, name, popularity, rating
    - edit filter buat category only
    - ilangin search bar
    - nambahin asc desc untuk sortby name
    - tapi pas masuk ke route product (sortby & filtering) foto & detail product nya malah ga keluar :D

--- e-commerce/app/Http/Controllers/productController.php
+++ e-commerce/app/Http/Controllers/productController.php
@@ -5,56 +5,64 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class productController extends Controller
+class ProductController extends Controller
 {
     public function index(Request $request)
     {
         $query = $request->input('query');
-        $sortBy = $request->input('sort_by', 'rating'); //default : sort by rating
+        $category = $request->input('category');
+        $sortBy = $request->input('sort_by', 'rating'); // default : sort by rating
+        $sortDirection = $request->input('sort_direction', 'asc'); // default : descending
 
         $products = Product::query();
 
         if ($query) {
-            $products = $products->where('name', 'LIKE', "%{$query}%");
+            $products = $products->where(function($q) use ($query) {
+                $q->where('name', 'LIKE', "%{$query}%")
+                  ->orWhere('category', 'LIKE', "%{$query}%");
+            });
+        }
+
+        if ($category) {
+            $products = $products->where('category', $category);
         }
 
         switch ($sortBy) {
-            case 'category' :
-                $products = $products->orderBy('category');
+            case 'name':
+                $products = $products->orderBy('name', $sortDirection);
                 break;
-            case 'name' :
-                $products = $products->orderBy('name');
+            case 'rating':
+                $products = $products->orderBy('rating');
                 break;
-            case 'price' :
+            case 'popularity':
+                $products = $products->orderBy('sold_count');
+                break;
+            case 'price':
                 $products = $products->orderBy('price');
                 break;
-            case 'popularity' :
-                $products = $products->orderBy('sold_count', 'desc');
-                break;
-            case 'rating' :
-            default: 
-                $products = $products->orderBy('rating', 'desc');
+            default:
+                $products = $products->orderBy('rating');
                 break;
         }
 
         $products = $products->get();
 
-        // Mengirimkan data produk ke view 'home'
-        return view('product', ['products'=>$products]);
+        // Mengirimkan data produk ke view 'product'
+        return view('product', ['products' => $products]);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
             'name' => 'required',
-            'category'=>'required',
+            'category' => 'required',
             'price' => 'required',
-            'stock'=>'required',
+            'stock' => 'required',
             'description' => 'required',
-            'image_url'=>'required',
+            'image_url' => 'required',
         ]);
-        
+
         $product = Product::create($request->all());
         return redirect()->route('profile');
     }
-
 }
--- e-commerce/database/seeders/ProductSeeder.php
+++ e-commerce/database/seeders/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
     {
         // Contoh data produk
         Product::create([
-            'name' => 'Nike Putih',
+            'name' => 'White Shoes',
             'description' => 'Description for Product 1',
             'category' => 'Shoes',
             'stock' => '20',
@@ -25,13 +25,53 @@ class ProductSeeder extends Seeder
         ]);
 
         Product::create([
-            'name' => 'Nike Ijo',
+            'name' => 'Green Shoes',
             'description' => 'Description for Product 2',
             'category' => 'Shoes',
             'stock' => '20',
             'price' => 75.00,
             'image_url' => 'images/image2.png',
             // 'category_id' => 2,
+        ]);
+
+        Product::create([
+            'name' => 'Black Hoodie',
+            'description' => 'Description for Product 3',
+            'category' => 'Shirts',
+            'stock' => '10',
+            'price' => 60.00,
+            'image_url' => 'images/image3.png',
+            // 'category_id' => 3,
+        ]);
+        
+        Product::create([
+            'name' => 'Black Sweatpants',
+            'description' => 'Description for Product 4',
+            'category' => 'Pants',
+            'stock' => '30',
+            'price' => 70.00,
+            'image_url' => 'images/image4.png',
+            // 'category_id' => 4,
+        ]);
+        
+        Product::create([
+            'name' => 'Black T-Shirt',
+            'description' => 'Description for Product 5',
+            'category' => 'Shirts',
+            'stock' => '25',
+            'price' => 45.00,
+            'image_url' => 'images/image5.png',
+            // 'category_id' => 5,
+        ]);
+        
+        Product::create([
+            'name' => 'Brown T-Shirt',
+            'description' => 'Description for Product 6',
+            'category' => 'Shirts',
+            'stock' => '35',
+            'price' => 45.00,
+            'image_url' => 'images/image6.png',
+            // 'category_id' => 6,
         ]);
     }
 }
