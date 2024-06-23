982ddd909005420101f4d91a24f10779855b2cbf
James Anderson <jamesandrn@gmail.com>
2024-06-23 23:02:31 +0700

    nampilin kategori di home sama di productdetail

--- e-commerce/resources/views/home.blade.php
+++ e-commerce/resources/views/home.blade.php
@@ -39,6 +39,8 @@ Home Content
                         <div class="text-center">
                             <!-- Product name-->
                             <h5 class="fw-bolder">{{ $product->name }}</h5>
+                            <!-- Product Category-->
+                            <h6>{{ $product->category}}</h3>
                             <!-- Product price-->
                             <span>Rp {{ $product->price }}</span>
                         </div>
--- e-commerce/resources/views/productdetail.blade.php
+++ e-commerce/resources/views/productdetail.blade.php
@@ -15,6 +15,7 @@
             <div class="col-md-6">
                 <h1 class="display-5 fw-bolder">{{ $product->name }}</h1>
                 <div class="fs-5 mb-5">
+                <h6>{{ $product->category}}</h3>
                     <!-- <span class="text-decoration-line-through">$45.00</span> -->
                     <span>Rp {{ $product->price }}</span>
                 </div>
