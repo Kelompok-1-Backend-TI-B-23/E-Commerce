eb00a2636f72b959a4df4587d1b481dcd78d735a
angel0060 <angelica.535230060@stu.untar.ac.id>
2024-06-27 21:40:40 +0700

    Menambah komen di admin controller dan purchase history controller

--- e-commerce/app/Http/Controllers/adminController.php
+++ e-commerce/app/Http/Controllers/adminController.php
@@ -13,21 +13,25 @@ class adminController extends Controller
 
     public function index(Request $request)
     {
+        // Mengarahkan ke view 'adminDashboard' 
         return view("adminDashboard");
     }
 
     public function indexProduct(Request $request)
     {
+        // Mengambil data produk dari database (diurutkan berdasarkan id produk, ascending)
         $products = Product::orderBy('id', 'asc')->get();
+
+        // Mengarahkan ke view 'adminProduct' dengan data produk
         return view('adminProduct', compact('products'));
     }
 
     public function indexTransaction(Request $request)
     {
-        // Mengambil semua data transaksi dari database
+        // Mengambil semua data transaksi dan usernya dari database
         $history = PurchaseHistory::all()->load('user', 'transaction');
  
-        // Mengirimkan data produk dan saldo eWallet ke view 'home'
+        // Mengirimkan data produk dan saldo eWallet ke view 'adminPurchase'
         return view('adminPurchase', compact('history'));
     }
 
--- e-commerce/app/Http/Controllers/purchaseHistoryController.php
+++ e-commerce/app/Http/Controllers/purchaseHistoryController.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Auth;
 class purchaseHistoryController extends Controller
 {
     public function index(){
+        
+        // Mengambil data riwayat transaksi berdasarkan user yang sedang login
         $user = Auth::user()->load('history.transaction');
+
+        // Mengarahkan ke view 'purchaseHistory' dengan data riwayat transaksi yang dilakukan user
         return view('purchaseHistory', compact('user'));
     }
 }
